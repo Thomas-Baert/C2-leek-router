ba26ffa1bed6ce7c902e623e82b1c3a04cb37391
Thomas BAERT <thomas.baert2014@gmail.com>
2026-04-23 15:11:07 +0200

    feat: setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
-#include <string>
+#include "network/peer/peer_table.hpp"
 
 int main(int argc, char** argv) {
-    std::cout << "Hello World!" << std::endl;
+    PeerTable peerTable;
+    peerTable.add_peer(1, "127.0.0.1");
+    peerTable.add_peer(2, "127.0.0.2");
+    std::cout << peerTable << std::endl;
     return 0;
 }
