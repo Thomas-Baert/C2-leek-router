0fd0651550ba328fad1cb9bee32d5da1a76dcd7e
Thomas BAERT <thomas.baert2014@gmail.com>
2026-04-24 15:57:53 +0200

    feat: allowing multiple handlers per server

--- src/network/handelers/abstract_handeler.hpp
+++ src/network/handelers/abstract_handeler.hpp
@@ -5,8 +5,6 @@
 #define ABSTRACT_HANDELER_HPP
 
 class AbstractHandeler {
-    std::unique_ptr<AbstractHandeler> previous;
-public:
     virtual ~AbstractHandeler() = default;
     virtual asio::awaitable<void> handle_client(asio::ip::tcp::socket socket) = 0;
 };
