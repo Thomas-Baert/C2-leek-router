69683b90fa307069187b9eb43c9375cc3884686f
Thomas Baert <thomas.baert@etu.umontpellier.fr>
2026-04-26 03:37:40 +0200

    refactor: suppress warnings

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
 #include "network/server.hpp"
 #include "network/handelers/Rebound.hpp"
 
-int main(int argc, char** argv) {
-    auto rebound = std::make_shared<Rebound>(12000);
-    auto rebound2 = std::make_shared<Rebound>(12002);
-    Server server = Server(12001);
+int main() {
+    const auto rebound = std::make_shared<Rebound>(12000);
+    const auto rebound2 = std::make_shared<Rebound>(12002);
+    auto server = Server(12001);
     server.add_handlers(rebound, rebound2);
     server.start(); 
 }
