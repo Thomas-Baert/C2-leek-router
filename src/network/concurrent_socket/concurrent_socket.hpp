b0406c20e3a6c225052e92e3a4ed5edb97997711
Thomas Baert <thomas.baert@etu.umontpellier.fr>
2026-04-26 03:37:10 +0200

    feat: total revamp of the concurrent socket

--- src/network/concurrent_socket/concurrent_socket.hpp
+++ src/network/concurrent_socket/concurrent_socket.hpp
@@ -1,51 +1,69 @@
 #pragma once
-#include <array>
-#include <algorithm>
-#include <vector>
+#include "ring_buffer.hpp"
+#include <asio.hpp>
+#include "config.h"
 
-class RingBuffer {
-    static constexpr size_t CAPACITY = 1024 * 1024; // 1MB
+using asio::ip::tcp;
 
-    std::array<char, CAPACITY> buffer;
-
-    size_t write_pos = 0;
-    size_t size = 0;
+class ConcurrentSocket {
+  RingBuffer ring_buffer;
+  tcp::socket socket;
+  std::array<std::atomic_size_t, MAX_CLIENT_CONNECTIONS> connections;
+  size_t current_connections;
+  std::mutex connections_mutex;
 
 public:
-    size_t write(const char* data, size_t len) {
-        size_t written = 0;
+  explicit ConcurrentSocket(tcp::socket& socket): socket(std::move(socket)) {
+    connections = std::array<std::atomic_size_t, MAX_CLIENT_CONNECTIONS>();
+    current_connections = 0;
+  }
 
-        while (written < len && size < CAPACITY) {
-            buffer[write_pos] = data[written];
+  size_t connect() {
+    std::lock_guard lock(connections_mutex);
+    const size_t new_id = current_connections;
+    if (new_id >= MAX_CLIENT_CONNECTIONS) return -1;
+    current_connections ++;
+    connections[new_id] = 0;
+    return new_id;
+  }
 
-            write_pos = (write_pos + 1) % CAPACITY;
-            written++;
-            size++;
-        }
+  size_t read(const int id, const size_t len, char buffer[]) {
+    std::lock_guard lock(connections_mutex);
+    const size_t number_read = ring_buffer.read(buffer, len, connections[id]);
+    connections[id] += number_read;
+    ring_buffer.update_reading_pointer((ring_buffer.get_reading_ptr() + connections[get_id_last()]) % CAPACITY);
+    if (number_read > 0) update_cursors();
+    return number_read;
+  }
 
-        return written;
+  void free_connection(const size_t id) {
+    std::lock_guard lock(connections_mutex);
+    connections[id] = CAPACITY + 1;
+  }
+
+private:
+  size_t get_id_last() const {
+    size_t id = 0;
+    size_t cursor = CAPACITY;
+
+    for (int i = 0; i < current_connections; ++i) {
+      if (connections[i] < cursor) {
+        cursor = connections[i];
+        id = i;
+      }
     }
+    return id;
+  }
 
-    size_t read(char* out,
-                size_t len,
-                size_t cursor) const {
-
-        if (cursor >= size)
-            return 0;
-
-        size_t available = size - cursor;
-        size_t to_read = std::min(len, available);
-
-        size_t pos = (write_pos + CAPACITY - size + cursor) % CAPACITY;
-
-        for (size_t i = 0; i < to_read; i++) {
-            out[i] = buffer[(pos + i) % CAPACITY];
-        }
-
-        return to_read;
+  void update_cursors() {
+    const size_t cursor_last = connections[get_id_last()];
+    for (int i = 0; i < current_connections; ++i) {
+      connections[i] -= cursor_last;
     }
+  }
 
-    size_t available() const {
-        return size;
-    }
+  // Threaded routine. It updates the buffer with the sockets messages
+  /*
+  void routine() {
+  }*/
 };
