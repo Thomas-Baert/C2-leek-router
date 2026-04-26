22c173eb1e7031c2dc993e745851e6e6a1ae7851
Thomas Baert <thomas.baert@etu.umontpellier.fr>
2026-04-26 03:36:27 +0200

    refactor: specification of visibility and suppression of useless include guards

--- src/network/handelers/abstract_handeler.hpp
+++ src/network/handelers/abstract_handeler.hpp
@@ -1,12 +1,9 @@
+#pragma once
 #include <asio.hpp>
 #include <asio/awaitable.hpp>
 
-#ifndef ABSTRACT_HANDELER_HPP
-#define ABSTRACT_HANDELER_HPP
-
 class AbstractHandeler {
+public:
     virtual ~AbstractHandeler() = default;
     virtual asio::awaitable<void> handle_client(asio::ip::tcp::socket socket) = 0;
 };
-
-#endif
